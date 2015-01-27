f8aca1d7f82621765c2740c4991c945a6960becc
cone <coneramu@gmail.com>
2015-01-27 11:26:23 -0600

    Adding Row Parser function
    Impact: Trivial

--- interfaces/repositories/row_parser.go
+++ interfaces/repositories/row_parser.go
@@ -1,7 +1,17 @@
 package repositories
 
-type RowParser struct{}
+import (
+	"reflect"
+)
 
-func (this *RowParser) Parse(row []interface{}) {
+func ParseRow(row []interface{}, target interface{}) {
+	s := reflect.ValueOf(target).Elem()
 
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+
+		strVal := reflect.ValueOf(row[i])
+
+		f.Set(strVal)
+	}
 }
--- interfaces/repositories/variant.go
+++ interfaces/repositories/variant.go
@@ -12,12 +12,40 @@ func NewVariantRepo() *VariantRepo {
 	}
 }
 
-func (this *VariantRepo) FindByProductId(productId int64) *models.Variant {
+func (this *VariantRepo) FindByProductId(productId int64) ([]*models.Variant, error) {
 	variant := &models.Variant{
 		ProductId: productId,
 	}
 
-	this.dbHandler.Find(variant)
+	variantSlice := []*models.Variant{}
 
-	return variant
+	rows, err := this.dbHandler.Find(variant).Rows()
+	if err != nil {
+		return nil, err
+	}
+
+	cols, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+
+	rawResult := make([][]byte, len(cols))
+
+	dest := make([]interface{}, len(cols)) // A temporary interface{} slice
+
+	for i, _ := range rawResult {
+		dest[i] = &rawResult[i] // Put pointers to each string in the interface slice
+	}
+
+	for rows.Next() {
+		rows.Scan(dest...)
+
+		newVariant := &models.Variant{}
+
+		ParseRow(rawResult, newVariant)
+
+		variantSlice = append(variantSlice, newVariant)
+	}
+
+	return variantSlice, nil
 }
