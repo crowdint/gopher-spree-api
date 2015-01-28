2c9675f3781eeaa06ff92882a670b454fc174418
Mumo Carlos <mumo.crls@gmail.com>
2015-01-28 15:47:40 -0600

    Add Asset repository
    * Add asset repository with FindByWieableId function

---
+++ interfaces/repositories/asset.go
@@ -0,0 +1,19 @@
+package repositories
+
+import "github.com/crowdint/gopher-spree-api/domain/models"
+
+type AssetRepo DbRepo
+
+func NewAssetRepo() *AssetRepo {
+  return &AssetRepo{
+    dbHandler: spree_db,
+  }
+}
+
+func (this *AssetRepo) FindByViewableIds(viewableIds []int64) []*models.Asset {
+  var assets []*models.Asset
+
+  this.dbHandler.Where("viewable_id in (?)", viewableIds).Find(&assets)
+
+  return assets
+}
---
+++ interfaces/repositories/asset_test.go
@@ -0,0 +1,40 @@
+package repositories
+
+import (
+  "os"
+  "reflect"
+  "testing"
+)
+
+func TestAssetRepo(t *testing.T) {
+  os.Setenv(dbUrlEnvName, "dbname=spree_dev sslmode=disable")
+  os.Setenv(dbEngineEnvName, "postgres")
+
+  err := InitDB()
+
+  if err != nil {
+    t.Error("An error has ocurred", err)
+  }
+
+  if spree_db == nil {
+    t.Error("Database helper not initialized")
+  }
+
+  defer spree_db.Close()
+
+  assetRepo := NewAssetRepo()
+
+  assets := assetRepo.FindByViewableIds([]int64{11})
+
+  nv := len(assets)
+
+  if nv < 1 {
+    t.Error("Invalid number of assets: %d", nv)
+  }
+
+  temp := reflect.ValueOf(*assets[0]).Type().String()
+
+  if temp != "models.Asset" {
+    t.Error("Invalid type", t)
+  }
+}
