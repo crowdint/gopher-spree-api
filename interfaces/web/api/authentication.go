6b1138ee3ac1a1e3dc8dcf0ea0adef0a781e7a2a
Ignacio Galindo & Isay Sosa <ignacio+isay@crowdint.com>
2015-01-28 17:45:35 -0600

    Validate user authentication by spree token

--- interfaces/web/api/authentication.go
+++ interfaces/web/api/authentication.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/crowdint/gopher-spree-api/domain/models"
+	"github.com/crowdint/gopher-spree-api/interfaces/repositories"
 )
 
 const (
@@ -18,14 +21,31 @@ func Authentication() gin.HandlerFunc {
 			return
 		}
 
-		if spreeToken := getSpreeToken(c); len(spreeToken) > 0 {
-			c.Set(SPREE_TOKEN, spreeToken)
-			c.Next()
-		} else {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "You must specify an API key."})
-			c.Abort(-1) // If abort index is lower than 0 header is not written
+		spreeToken := getSpreeToken(c)
+
+		// Return if spree token is not provided
+		if spreeToken == "" {
+			unauthorized(c, "You must specify an API key.")
+			return
 		}
+
+		user := &models.User{}
+		err := repositories.NewDatabaseRepository().FindBy(user, map[string]interface{}{"spree_api_key": spreeToken})
+
+		if err != nil {
+			unauthorized(c, "Invalid API key ("+spreeToken+") specified.")
+			return
+		}
+
+		c.Set(SPREE_TOKEN, spreeToken)
+		c.Set("CurrentUser", user)
+		c.Next()
 	}
+}
+
+func unauthorized(c *gin.Context, errMsg string) {
+	c.JSON(http.StatusUnauthorized, gin.H{"error": errMsg})
+	c.Abort(-1) // If abort index is lower than 0 header is not written
 }
 
 func getSpreeToken(c *gin.Context) string {
