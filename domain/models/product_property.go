7cf4a246a326ba1091e0652ce2c12887d25793f3
Mumo Carlos <mumo.crls@gmail.com>
2015-01-30 14:09:53 -0600

    Format product property model

--- domain/models/product_property.go
+++ domain/models/product_property.go
@@ -3,13 +3,14 @@ package models
 import "time"
 
 type ProductProperty struct {
-	Id         int64
-	Value      string
-	ProductId  int64
-	PropertyId int64
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-	Position   int64
+	Id           int64
+	Value        string
+	ProductId    int64
+	PropertyId   int64
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	Position     int64
+	PropertyName string
 }
 
 func (this ProductProperty) TableName() string {
