267ea70d8aa1997c20d8cc82ae2450abcd93c112
Mumo Carlos <mumo.crls@gmail.com>
2015-01-30 09:57:00 -0600

    Update variant sql formatting
    * Makes update for legilibility and consistency

--- interfaces/repositories/variant.go
+++ interfaces/repositories/variant.go
@@ -16,13 +16,13 @@ func (this *VariantRepo) FindByProductIds(productIds []int64) ([]*models.Variant
 
 	variants := []*models.Variant{}
 
-	sqlStr := "select spree_variants.*, sum(count_on_hand) AS real_stock_items_count, spree_stock_items.backorderable, spree_prices.amount price " +
-		"from spree_variants " +
-		"inner join spree_stock_items on spree_variants.id = spree_stock_items.variant_id " +
-		"inner join spree_prices on spree_variants.id = spree_prices.variant_id " +
-		"where spree_prices.currency='USD' " +
+	sqlStr := "SELECT spree_variants.*, SUM(count_on_hand) AS real_stock_items_count, spree_stock_items.backorderable, spree_prices.amount price " +
+		"FROM spree_variants " +
+		"INNER JOIN spree_stock_items ON spree_variants.id = spree_stock_items.variant_id " +
+		"INNER JOIN spree_prices ON spree_variants.id = spree_prices.variant_id " +
+		"WHERE spree_prices.currency='USD' " +
 		"AND spree_variants.product_id IN (?) " +
-		"group by spree_variants.id, spree_variants, backorderable, price"
+		"GROUP BY spree_variants.id, spree_variants, backorderable, price"
 
 	query := this.dbHandler.Raw(sqlStr, productIds).Scan(&variants)
 
