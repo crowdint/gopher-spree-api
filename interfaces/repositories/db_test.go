de848bf3e23f3147e6388e54a1b0d234b0352d42
Ignacio Galindo & Isay Sosa <ignacio+isay@crowdint.com>
2015-02-02 13:22:22 -0600

    Add database All() finder to repository with tests

--- interfaces/repositories/db_test.go
+++ interfaces/repositories/db_test.go
@@ -2,6 +2,8 @@ package repositories
 
 import (
 	"testing"
+
+	"github.com/crowdint/gopher-spree-api/domain/models"
 )
 
 func TestDB(t *testing.T) {
@@ -16,4 +18,70 @@ func TestDB(t *testing.T) {
 	}
 
 	Spree_db.Close()
+}
+
+func TestAll(t *testing.T) {
+	InitDB()
+
+	r := NewDatabaseRepository()
+
+	p := &[]models.Product{}
+
+	err := r.All(p, nil)
+
+	if err != nil {
+		t.Errorf("DB.All %s", err)
+	}
+
+	if len(*p) == 0 {
+		t.Errorf("DB.All should have found results")
+	}
+}
+
+func TestAllWithConditions(t *testing.T) {
+	InitDB()
+
+	r := NewDatabaseRepository()
+
+	p := &[]models.Product{}
+
+	err := r.All(p, map[string]interface{}{"id": 1})
+
+	if err != nil {
+		t.Errorf("DB.All %s", err)
+	}
+
+	if len(*p) != 1 {
+		t.Errorf("DB.All should not have found more than one result")
+	}
+}
+
+func TestFindBy(t *testing.T) {
+	InitDB()
+
+	r := NewDatabaseRepository()
+
+	p := &models.Product{}
+
+	err := r.FindBy(p, nil)
+
+	if err != nil {
+		t.Errorf("DB.All %s", err)
+	}
+}
+
+func TestFindByWithConditions(t *testing.T) {
+	InitDB()
+
+	r := NewDatabaseRepository()
+
+	p := &models.Product{}
+
+	err := r.FindBy(p, map[string]interface{}{
+		"id": 1,
+	})
+
+	if err != nil {
+		t.Errorf("DB.All %s", err)
+	}
 }
