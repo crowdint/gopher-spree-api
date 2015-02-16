60da541b75592011b390231389897c062fcf2cb4
Ignacio Galindo & Isay Sosa <ignacio+isay@crowdint.com>
2015-02-16 16:34:36 -0600

    Get data for line items and sub resources
    - variant: products
    - prices
    - currency
    - stock locations
    - stock items

--- usecases/json/order.go
+++ usecases/json/order.go
@@ -3,9 +3,11 @@ package json
 import (
 	"github.com/jinzhu/copier"
 
+	"github.com/crowdint/gopher-spree-api/configs/spree"
 	"github.com/crowdint/gopher-spree-api/domain/json"
 	"github.com/crowdint/gopher-spree-api/domain/models"
 	"github.com/crowdint/gopher-spree-api/interfaces/repositories"
+	. "github.com/crowdint/gopher-spree-api/utils"
 )
 
 type OrderInteractor struct {
@@ -23,6 +25,24 @@ func (this *OrderInteractor) Show(o *models.Order, u *models.User) (*json.Order,
 	order.BillAddress = this.getAddress(&order, "BillAddressId")
 	order.ShipAddress = this.getAddress(&order, "ShipAddressId")
 	order.LineItems = this.getLineItems(&order)
+
+	variantIds := Collect(*order.LineItems, "VariantId")
+	var variants []json.Variant
+	this.Repository.AllBy(&variants, nil, "id IN(?)", variantIds)
+
+	productIds := Collect(variants, "ProductId")
+	var products []json.Product
+	this.Repository.AllBy(&products, nil, "id IN(?)", productIds)
+
+	var prices []models.Price
+	this.Repository.AllBy(&prices, repositories.Q{"currency": spree.Get(spree.SPREE_CURRENCY)}, "variant_id IN(?)", variantIds)
+
+	var stockLocations []json.StockLocation
+	this.Repository.All(&stockLocations, repositories.Q{"active": true})
+	stockLocationIds := Collect(stockLocations, "Id")
+
+	var stockItems []json.StockItem
+	this.Repository.AllBy(&stockItems, nil, "variant_id IN(?) AND stock_location_id IN(?)", variantIds, stockLocationIds)
 
 	return &order, nil
 }
