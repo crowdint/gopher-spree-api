1f7105390bc9c4144c86432d38b6045d21c9934d
Ignacio Galindo & Isay Sosa <ignacio+isay@crowdint.com>
2015-02-16 19:31:14 -0600

    Add utils.ToMap
    - Returns a map of property values/slice values

--- utils/utils.go
+++ utils/utils.go
@@ -13,3 +13,28 @@ func Collect(collection interface{}, field string) (result []interface{}) {
 	}
 	return
 }
+
+func ToMap(collection interface{}, key string, multiple bool) map[int64]interface{} {
+	slice := reflect.ValueOf(collection)
+
+	result := make(map[int64]interface{})
+
+	for i := 0; i < slice.Len(); i++ {
+		key := slice.Index(i).FieldByName(key).Int()
+
+		if multiple {
+			newValue := slice.Index(i)
+
+			if result[key] == nil {
+				result[key] = []interface{}{newValue.Interface()}
+			} else {
+				result[key] = append(result[key].([]interface{}), newValue.Interface())
+			}
+
+		} else {
+			result[key] = slice.Index(i).Interface()
+		}
+	}
+
+	return result
+}
