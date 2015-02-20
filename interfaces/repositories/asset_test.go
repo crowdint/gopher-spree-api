3cce872841973e1c95512d0c17c4491ccc008df3
Ignacio Galindo & Isay Sosa <ignacio+isay@crowdint.com>
2015-02-20 13:49:53 -0600

    Adding tests
    * Adding a test for OptionValueRepo.AllByVariantAssociation(variant).
    * Adding a test for AssetInteractor.toJsonAssets([]*models.Asset).
    * Adding a test for AssetRepo.AllImagesByVariantId(int64).

--- interfaces/repositories/asset_test.go
+++ interfaces/repositories/asset_test.go
@@ -38,3 +38,28 @@ func TestAssetRepo(t *testing.T) {
 		t.Error("Invalid type", t)
 	}
 }
+
+func TestAssetRepo_AllImagesByVariantId(t *testing.T) {
+	err := InitDB()
+
+	if err != nil {
+		t.Error("An error has ocurred", err)
+	}
+
+	if Spree_db == nil {
+		t.Error("Database helper not initialized")
+	}
+
+	defer Spree_db.Close()
+
+	assetRepo := NewAssetRepo()
+
+	images, err := assetRepo.AllImagesByVariantId(11)
+	if err != nil {
+		t.Error("There was an error", err)
+	}
+
+	if len(images) < 1 {
+		t.Errorf("There aren't images for this variant: %d", 11)
+	}
+}
