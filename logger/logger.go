28621e63da5ad517007897182da4ad1634c0385f
Herman Moreno <herman.moreno@crowdint.com>
2015-04-21 15:57:48 -0500

    Create a logger wrapper with a logrus implementation

---
+++ logger/logger.go
@@ -0,0 +1,67 @@
+package logger
+
+type Logger interface {
+	Debug(args ...interface{})
+	Info(args ...interface{})
+	Warn(args ...interface{})
+	Error(args ...interface{})
+	Fatal(args ...interface{})
+
+	Debugf(format string, args ...interface{})
+	Infof(format string, args ...interface{})
+	Warnf(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fatalf(format string, args ...interface{})
+
+	SetLevel(lvl string)
+}
+
+var log Logger
+
+func SetLogger(newLogger Logger) {
+	log = newLogger
+}
+
+func SetLevel(lvl string) {
+	log.SetLevel(lvl)
+}
+
+func Debug(args ...interface{}) {
+	log.Debug(args...)
+}
+
+func Info(args ...interface{}) {
+	log.Info(args...)
+}
+
+func Warn(args ...interface{}) {
+	log.Warn(args...)
+}
+
+func Error(args ...interface{}) {
+	log.Error(args...)
+}
+
+func Fatal(args ...interface{}) {
+	log.Fatal(args...)
+}
+
+func Debugf(format string, args ...interface{}) {
+	log.Debugf(format, args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	log.Infof(format, args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	log.Warnf(format, args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	log.Errorf(format, args...)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	log.Fatalf(format, args...)
+}
